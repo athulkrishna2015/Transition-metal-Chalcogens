691e394073ba3ce5989a70f97a2e1d26dadea6c5
athulkrishna2015 <athulkrishnasv2015@gmail.com>
2022-06-07 02:51:41 +0530

    adding files

---
+++ NN/properties.py
@@ -0,0 +1,58 @@
+2Dpfx
+2Dpfy
+area
+CBEMD1
+CBEMD2
+CBM_GW
+CBM_HSE06
+CBM
+Dgap_Gw
+Dgap_HSE06
+Dgap
+Dgap_WSOC
+EBE
+Eng
+gap
+gp_gw
+gp_HSE06
+GWSOC
+Natom
+PDS_class
+PDS_reg
+TDSL_class
+TDSL_reg
+VBEMD1
+VBEMD2
+VBM_GW
+VBM_HSE06
+VBM
+VL
+
+cp 'NN_all.ipynb' 'NN_all_2Dpfx.ipynb'
+cp 'NN_all.ipynb' 'NN_all_2Dpfy.ipynb'
+cp 'NN_all.ipynb' 'NN_all_area.ipynb'
+cp 'NN_all.ipynb' 'NN_all_CBEMD1.ipynb'
+cp 'NN_all.ipynb' 'NN_all_CBEMD2.ipynb'
+cp 'NN_all.ipynb' 'NN_all_CBM_GW.ipynb'
+cp 'NN_all.ipynb' 'NN_all_CBM_HSE06.ipynb'
+cp 'NN_all.ipynb' 'NN_all_CBM.ipynb'
+cp 'NN_all.ipynb' 'NN_all_Dgap_Gw.ipynb'
+cp 'NN_all.ipynb' 'NN_all_Dgap_HSE06.ipynb'
+cp 'NN_all.ipynb' 'NN_all_Dgap.ipynb'
+cp 'NN_all.ipynb' 'NN_all_Dgap_WSOC.ipynb'
+cp 'NN_all.ipynb' 'NN_all_EBE.ipynb'
+cp 'NN_all.ipynb' 'NN_all_Eng.ipynb'
+cp 'NN_all.ipynb' 'NN_all_gap.ipynb'
+cp 'NN_all.ipynb' 'NN_all_gp_gw.ipynb'
+cp 'NN_all.ipynb' 'NN_all_gp_HSE06.ipynb'
+cp 'NN_all.ipynb' 'NN_all_GWSOC.ipynb'
+cp 'NN_all.ipynb' 'NN_all_Natom.ipynb'
+cp 'NN_all.ipynb' 'NN_all_PDS.ipynb'
+cp 'NN_all.ipynb' 'NN_all_TDSL.ipynb'
+cp 'NN_all.ipynb' 'NN_all_VBEMD1.ipynb'
+cp 'NN_all.ipynb' 'NN_all_VBEMD2.ipynb'
+cp 'NN_all.ipynb' 'NN_all_VBM_GW.ipynb'
+cp 'NN_all.ipynb' 'NN_all_VBM_HSE06.ipynb'
+cp 'NN_all.ipynb' 'NN_all_VBM.ipynb'
+cp 'NN_all.ipynb' 'NN_all_VL.ipynb'
+
---
+++ material gen/MX1nX2nX3n.py
@@ -0,0 +1,11 @@
+M = ['Sc','Ti','V','Cr','Mn','Fe','Co','Ni','Cu','Zn','Y','Zr','Nb','Mo','Tc','Ru','Rh','Pd','Ag','Cd','Lu','Hf','Ta','W','Re','Os','Ir','Pt','Au','Hg','Lr','Rf','Db','Sg','Bh','Hs','Mt','Ds','Rg','Cn']
+X = ['O','S','Se','Te','Po','Lv']
+for m in range(len(M)):
+    for x0 in range(len(X)):
+        for x1 in range(len(X)):
+            for x2 in range(len(X)):
+                for i0 in range (1,10):
+                    for i1 in range(1,10):
+                        for i2 in range(1,10):
+                            if (x0 != x1) and (x1 != x2) and (x0 != x2):
+                                print(f'{M[m]}{X[x0]}{i0}{X[x1]}{i1}{X[x2]}{i2}')   
